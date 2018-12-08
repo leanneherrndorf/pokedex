3f1cb44b35ed448a0e4e8ca8b0563d86be29ba45
leanneherrndorf <leanneherrndorf@gmail.com>
2018-12-08 14:49:09 -0800

    Use a cleaner way to loop over list

--- src/components/List.js
+++ src/components/List.js
@@ -4,8 +4,8 @@ import Card from './Card'
 class List extends Component {
 	render() {
 		return <div className="list">
-			{ Object.keys(this.props.items).map((key) =>
-				<Card key={key} item={this.props.items[key]} findPokemon={this.props.findPokemon}></Card>
+			{ this.props.items.map((value, key) =>
+				<Card key={key} item={value} findPokemon={this.props.findPokemon}></Card>
 			)}
 		</div>;
 	}
