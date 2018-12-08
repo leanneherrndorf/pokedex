2634c860069b15e702356d7f76aa1e3bd29e175f
leanneherrndorf <leanneherrndorf@gmail.com>
2018-12-08 13:07:23 -0800

    Use the Pokemon API to grab a list of all pokemons from the kanto deck

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 
 class App extends Component {
 	render() {
+	getPokemonList = () => {
+		fetch(`http://pokeapi.salestock.net/api/v2/pokedex/2`)
+			.then(result => result.json())
+			.then(data => {
+				this.setState({ items: data.pokemon_entries})
+			})
+			.catch(error => {
+				console.log(error)
+			})
+	}
+
+	constructor() {
+		super();
+		this.state = {
+			items: []
+		};
+	}
+
+	componentDidMount = () => {
+		this.getPokemonList()
+	}
 		return (
 			<div className="App">
 				<header className="App-header">
