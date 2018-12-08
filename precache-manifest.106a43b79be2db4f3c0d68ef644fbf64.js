13e300ffa83658c643c30160f544aa3ac898972e
leanneherrndorf <leanneherrndorf@gmail.com>
2018-12-08 13:41:28 -0800

    Updates

---
+++ precache-manifest.106a43b79be2db4f3c0d68ef644fbf64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69bab70dd00f4429b781",
+    "url": "/pokedex/static/js/runtime~main.69bab70d.js"
+  },
+  {
+    "revision": "d6c63f558c4654ee940a",
+    "url": "/pokedex/static/js/main.d6c63f55.chunk.js"
+  },
+  {
+    "revision": "2db506eb964d84f9c86a",
+    "url": "/pokedex/static/js/1.2db506eb.chunk.js"
+  },
+  {
+    "revision": "d6c63f558c4654ee940a",
+    "url": "/pokedex/static/css/main.654de578.chunk.css"
+  },
+  {
+    "revision": "ca3ca299c3fb42d28f853eeea1bc3f5d",
+    "url": "/pokedex/index.html"
+  }
+];
