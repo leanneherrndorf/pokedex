a917f5154a74b052d31527bc627ffc2b16e46fca
leanneherrndorf <leanneherrndorf@gmail.com>
2018-12-08 16:28:11 -0800

    Create a CardWrapper styled component

--- src/components/PokeData.js
+++ src/components/PokeData.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const CardWrapper = styled.div`
+	display: inline-block;
+	position: fixed;
+	right: 0;
+	margin: 20px 40px;
+	padding: 30px;
+	text-align: center;
+	border: 1px solid grey;
+	background: #fdf8cf91;
+	max-width: 300px;
+
+	h2 {
+		margin: 0;
+	}
+	
+	img {
+		width: 200px;
+	}
+	
+	table {
+		width: 100%;
+	}
+
+	td:first-child {
+		text-align: left;
+		font-weight: 600;
+	}
+`
 
 class Card extends Component {
 	render() {
-		return <div className="details">
+		return <CardWrapper>
 			<h2>{this.props.pokemon.name}</h2>
 			<img alt= "" src={this.props.pokemon.sprite} />
 			<table>
@@ -23,7 +53,7 @@ class Card extends Component {
 					</tr>
 				</tbody>
 			</table>
-		</div>;
+		</CardWrapper>;
 	}
 }
 
