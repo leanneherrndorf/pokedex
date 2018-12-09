0a4eab4cbfbf89f57f16ff699f21a4deffdef389
leanneherrndorf <leanneherrndorf@gmail.com>
2018-12-08 16:19:08 -0800

    Use styled components createGlobalStyle and add in an AppWrapper

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import List from './List';
-import Pokemon from '../Pokemon'
-import PokeData from './PokeData'
-import '../style/App.css';
+import Pokemon from '../Pokemon';
+import PokeData from './PokeData';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+	body {
+		font-family: 'Courier New', Courier, monospace;
+		font-weight: 600;
+		background: aquamarine;
+	}
+	
+	h1 {
+		text-align: center;
+	}
+`;
+
+const AppWrapper = styled.div`
+	margin: 20px;
+`;
 
 class App extends Component {
 	getPokemonList = () => {
@@ -14,7 +30,7 @@ class App extends Component {
 			.catch(error => {
 				console.log(error)
 			})
-	}
+	};
 
 	constructor() {
 		super();
@@ -23,11 +39,11 @@ class App extends Component {
 			pokemon: '',
 			detailView: false
 		};
-	}
+	};
 
 	componentDidMount = () => {
 		this.getPokemonList()
-	}
+	};
 
 	findPokemon = (id) => {
 		fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
@@ -46,11 +62,12 @@ class App extends Component {
 
   render() {
 		return (
-				<div className="App">
-					<h1>Pokemon Pokedex</h1>
-					<List items={this.state.items} findPokemon={this.findPokemon}/>
-					{this.state.detailView && <PokeData pokemon={this.state.pokemon}/>}
-				</div>
+			<AppWrapper>
+				<GlobalStyle/>
+				<h1>Pokemon Pokedex</h1>
+				<List items={this.state.items} findPokemon={this.findPokemon}/>
+				{this.state.detailView && <PokeData pokemon={this.state.pokemon}/>}
+			</AppWrapper>
 		);
 	}
 }
